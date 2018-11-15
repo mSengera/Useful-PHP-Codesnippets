d5841fdef15439125019e1655b7833baf24606db
Marvin Sengera <work@marvin-sengera.de>
2018-11-15 09:38:06 +0100

    Create multiexplode.php

---
+++ multiexplode.php
@@ -0,0 +1,8 @@
+/*
+ * Extends PHP explode() to multiexplode() with multiple explode characters
+ */
+private function multiexplode ($delimiters,$string) {
+    $ready = str_replace($delimiters, $delimiters[0], $string);
+    $launch = explode($delimiters[0], $ready);
+    return  $launch;
+}
